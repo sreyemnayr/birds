c0c28c3d9b9754c038d7ac2e8b9d5d1997dbee7e
Ryan Meyers <ryan@Ryans-MacBook-Pro.local>
2023-05-03 01:22:53 -0500

    Get rid of console logs

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
 
@@ -19,15 +20,11 @@ export async function GET() {
           const tokens = db.collection("tokens")
     
           
-    
           const result = await tokens.find({minted:true}).sort({mint_block:-1}).limit(1).toArray()
           const mint_block = result[0].mint_block
-          console.log(result)
-          console.log(mint_block)
-
+          
           const res = await fetch(`https://api.etherscan.io/api?module=account&action=tokennfttx&address=0x8455c49C92c814DD293c6bE2888298E88B274007&page=0&offset=1000&startblock=${mint_block + 1}&sort=asc&apikey=M6A46XPIMK6R69D9C2M8PTAAMZ2KKDSAD7`).then(r=>r.json())
           
-          console.log(res)
 
           if(res){
             for(let tx of res.result){
@@ -36,6 +33,8 @@ export async function GET() {
           }
 
           await client.close();
+
+          return NextResponse.json({success: true})
     
           
         } catch (error) {
