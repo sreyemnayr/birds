3a64844e7d12e1a7db3cdf9a90a1f0f8f6c1433c
Ryan Meyers <ryan@Ryans-MacBook-Pro.local>
2023-05-03 01:15:00 -0500

    Fix cron

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -20,11 +20,15 @@ export async function GET() {
     
           
     
-          const result = await tokens.find().sort({mint_block:-1}).limit(1).toArray()
+          const result = await tokens.find({minted:true}).sort({mint_block:-1}).limit(1).toArray()
           const mint_block = result[0].mint_block
+          console.log(result)
+          console.log(mint_block)
 
           const res = await fetch(`https://api.etherscan.io/api?module=account&action=tokennfttx&address=0x8455c49C92c814DD293c6bE2888298E88B274007&page=0&offset=1000&startblock=${mint_block + 1}&sort=asc&apikey=M6A46XPIMK6R69D9C2M8PTAAMZ2KKDSAD7`).then(r=>r.json())
           
+          console.log(res)
+
           if(res){
             for(let tx of res.result){
                 await tokens.updateOne({token_id: parseInt(tx.tokenID)}, {$set: {minted: true, mint_tx: tx.hash, mint_block: parseInt(tx.blockNumber)}})
